0ced398034fe47b47c1d02ef659d2658eace5ce0
Amosgantian <97826100+Amosgantian@users.noreply.github.com>
2024-08-24 15:48:10 +0800

    Update recover_vmess.py

--- recover_vmess.py
+++ recover_vmess.py
@@ -49,9 +49,9 @@ summary_message = "serv00-vmess 恢复操作结果：\n"
 
 # 默认恢复命令列表
 default_restore_commands = [
-    "nohup /home/amoz/.npm-global/bin/pm2 start /home/amoz/nezhapanel/dashboard &",
-    "nohup /home/amoz/.npm-global/bin/pm2 start /home/amoz/nezhapanel/agent.sh &",
-    "nohup /home/amoz/.npm-global/bin/pm2 start /home/amoz/domains/poland.yhgenedit.us.kg/vless/app.js --name vless &",
+    "pgrep -x dashboard > /dev/null || nohup /home/amoz/.npm-global/bin/pm2 start /home/amoz/nezhapanel/dashboard --name dashboard --cwd /home/amoz/nezhapanel &",
+    "pgrep -x dashboard > /dev/null || nohup /home/amoz/.npm-global/bin/pm2 start /home/amoz/nezhapanel/agent.sh &",
+    "pgrep -x vless > /dev/null || nohup /home/amoz/.npm-global/bin/pm2 start /home/amoz/domains/poland.yhgenedit.us.kg/vless/app.js --name vless &",
     "nohup /home/yahaigene/.npm-global/bin/pm2 start /home/yahaigene/domains/xray/start.sh >/dev/null 2>&1 &",
     "nohup /home/menghunke/sb_00.sh >/dev/null 2>&1 &"
 ]
